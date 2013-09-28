8ede89abf23e5c2af40c2c15027250dbb8816b98
Chris Schneider <cschneider@comverge.com>
2013-09-28 13:26:06 -0600

    Connect redis, and solidify the interface a bit

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,102 @@
 import Job
 import Data.Aeson
 import Data.Maybe
-import Data.Map
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Char8 as SBS
+import Control.Monad (forever)
+
+import Database.Redis hiding (decode)
+
+type Queue = SBS.ByteString
+type JobHandler = Queue -> JobArgs -> IO ()
+
+data WorkerConfig = WorkerConfig { 
+                         name         :: String
+                       , queues       :: [(Queue, JobHandler)] -- | Process all the messages on the first queue before the next
+                       , numberToFork :: Int
+                       }
+
+instance Show WorkerConfig where
+  show WorkerConfig {name = name, queues = queues, numberToFork = numberToFork} = 
+    "WorkerConfig {name = \"" ++ name ++ "\", queues = " ++ unwords (map (SBS.unpack . fst) queues) ++ ", numberToFork = " ++ show numberToFork ++ "}"
+
+data ResqueConfig = ResqueConfig {
+                         host           :: String
+                       , port           :: Int
+                       , databaseNumber :: Int
+                       , password       :: Maybe BS.ByteString
+                       , prefix         :: String
+                       } deriving (Show)
+
+defaultResqueConfig :: ResqueConfig
+defaultResqueConfig = ResqueConfig { host = "localhost", port = 1000, databaseNumber = 1, password = Nothing, prefix = "resque:" }
 
 main :: IO ()
-main = printJobInfo $ fromJust (decode "{ \"class\": \"Email\", \"vars\": { \"to\": \"foo@example.com\", \"from\": \"steve@example.com\" } }" :: Maybe Job)
+main = do
+  let resqueConfig = defaultResqueConfig
+  forkResqueWorker resqueConfig controlEventWorkerConfig
 
-printJobInfo :: Job -> IO ()
-printJobInfo job = do
-  putStrLn $ "Class To Run: " ++ jobClass job
-  mapM_ (putStrLn . displayOne) (toList $ getJobArgs $ jobVars job)
+controlEventWorkerConfig :: WorkerConfig
+controlEventWorkerConfig = WorkerConfig {
+                             name = "ControlEventWorker"
+                           , queues = [("ControlEvent", controlEventHandler)]
+                           , numberToFork = 5
+                           }
+
+controlEventHandler :: JobHandler
+controlEventHandler _ jobConfig = putStrLn "Called me!"
+
+
+forkResqueWorker :: ResqueConfig -> WorkerConfig -> IO ()
+forkResqueWorker resqueConfig workerConfig = do
+  redisConn <- connect (resqueConfigToRedisConfig resqueConfig)
+
+  -- forever $ do
+  maybeJob <- getJobFromQueues redisConn (fst . head $ queues workerConfig)
+
+  putStrLn "Got this from redis:"
+  print maybeJob
+
+  case maybeJob of
+    Nothing  -> putStrLn "No Job Found"
+    Just job -> do
+      let parsedJob = parseJob job
+      putStrLn "Got parsed job: "
+      print parsedJob
+      runFunction
+
+
+-- Run some redis magic, pull the result back into IO monad
+getJobFromQueues :: Connection -> Queue -> IO (Maybe ByteString)
+getJobFromQueues conn queueName = do
+  answer <- getRedisAnswer
+  case answer of
+    Just x  -> return $ Just $ BS.fromChunks [x]
+    Nothing -> return Nothing
   where
-    displayOne (k,v) = k ++ " : " ++ v
+  getRedisAnswer = runRedis conn $ do
+                     maybeResult <- lpop queueName
+                     return $ case maybeResult of
+                       Left _  -> Nothing
+                       Right x -> x
 
+  -- "{ \"class\": \"Email\", \"vars\": { \"to\": \"foo@example.com\", \"from\": \"steve@example.com\" } }"
+
+-- Turn a bytestring into a parsed job object
+parseJob :: ByteString -> Job
+parseJob jobJson = fromJust (decode jobJson :: Maybe Job)
+
+-- And finally run the function that's appropriate
+runFunction :: IO ()
+runFunction = return ()
+
+resqueConfigToRedisConfig :: ResqueConfig -> ConnectInfo
+resqueConfigToRedisConfig ResqueConfig { host = host, port = port, databaseNumber = databaseNumber, password = password, prefix = prefix } =
+      defaultConnectInfo {
+        connectHost           = host
+      {- , connectPort           = PortNum port -}
+      {- , connectAuth           = password -}
+      , connectMaxConnections = 10
+      }
 
